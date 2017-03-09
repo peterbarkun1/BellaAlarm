021e637e036657bfc8ca42200aebece793e65a70
Peter <waffelk@mail.ru>
2017-03-09 19:28:40 +0300

    build 3.1

--- app/src/main/java/petersworkshop/animationbuttonplus/MainActivity.java
+++ app/src/main/java/petersworkshop/animationbuttonplus/MainActivity.java
@@ -1,14 +1,13 @@
 package petersworkshop.animationbuttonplus;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
         //нахождение элементов на xml файле
 
