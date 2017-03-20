8779d002cbc4a4e78ba6f9d86546e2ad61da6f0a
Peter <waffelk@mail.ru>
2017-03-20 12:28:51 +0300

    (alpha 0.3.7.1) work with Graph of Biorythm

--- app/src/main/java/petersworkshop/animationbuttonplus/Main2Activity.java
+++ app/src/main/java/petersworkshop/animationbuttonplus/Main2Activity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.transition.Explode;
 import android.view.Window;
+import android.widget.Toast;
 
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
@@ -12,7 +13,7 @@ import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.util.Calendar;
 import java.util.Date;
-import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -32,20 +33,51 @@ public class Main2Activity extends AppCompatActivity {
         initPage();
         initAnimation();
 //__________________________________________________________________________________________________
+
+
         //объявления переменных для графика и расчеты
 
         double x,y,x1,y1,pi;
-        Date ts;//дата сегодня
-
         pi=3.14;
-
-        ts = new Date();
-        System.out.println(ts);//дата сегодня
-
-        Calendar tr = new GregorianCalendar(2013, 8, 13);//левая дата рождения
 
 
 //__________________________________________________________________________________________________
+        Date ts = new Date();
+        System.out.println(ts);//дата сегодня
+        double millis_segodn = ts.getTime();
+        //Toast.makeText(this,(String.valueOf(millis)), Toast.LENGTH_LONG).show();
+        //во время открытия  активити выводится на экран количество  миллисекунд от 1970 до сегодня
+
+
+        TimeZone tz = TimeZone.getTimeZone("Europe/Moscow");
+
+        Calendar tr = Calendar.getInstance(tz);
+        tr.setLenient(false);
+
+        tr.set(Calendar.MILLISECOND, 0);
+        System.out.println(tr.getTimeInMillis());//обнуление
+
+        tr.set(1998, Calendar.AUGUST, 7, 12, 30, 0);//левая дата
+        System.out.println(tr.getTimeInMillis());
+
+        double millis_rozhden = tr.getTimeInMillis();
+
+        //Toast.makeText(this,(String.valueOf(millis1)), Toast.LENGTH_LONG).show();
+        // во время открытия, выводит к-во миллисеекунд от 1970 до 1998 августа 7-го 12 30
+
+        double raz = millis_segodn - millis_rozhden;
+        //Toast.makeText(this,(String.valueOf(raz)), Toast.LENGTH_LONG).show();
+        //во время открытия, выводит разницу миллисекунд сегодняшнего дня от 7 августа 98
+
+        double perev = raz/86400000;
+        Toast.makeText(this,(String.valueOf(perev)), Toast.LENGTH_LONG).show();
+        //переводит разницу из миллисекунд в дни
+
+        double[] razdney = new double[7];
+
+//__________________________________________________________________________________________________
+
+
         //отрисовка графиков
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
@@ -56,7 +88,7 @@ public class Main2Activity extends AppCompatActivity {
             for (int i = 0; i<7; i++)
             {
                 x=x+1;
-                y=Math.sin(2*x*x/23);
+                y=Math.sin(2*pi*x/23);
                 series.appendData(new DataPoint(x,y),true,7);
             }
         graph.addSeries(series);
@@ -82,7 +114,11 @@ public class Main2Activity extends AppCompatActivity {
             series.setDataPointsRadius(10);
             series.setThickness(8);
     }
+
+
 //__________________________________________________________________________________________________
+
+
     //анимация
     private void initPage() {
         type = (Constants.TransitionType) getIntent().getSerializableExtra(Constants.KEY_ANIM_TYPE);
